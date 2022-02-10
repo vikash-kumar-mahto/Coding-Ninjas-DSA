dd19ecc6da8cf02d20cdb79d8b44baac49ebbc81
vikash kumar mahto <93486699+vikash-kumar-mahto@users.noreply.github.com>
2022-02-10 12:06:29 +0530

    Create Return Keypad Code.cpp

---
+++ Recursion2/Return Keypad Code.cpp
@@ -0,0 +1,92 @@
+/*Return Keypad Code
+Send Feedback
+Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
+Return empty string for numbers 0 and 1.
+Note : 1. The order of strings are not important.
+2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
+Input Format :
+Integer n
+Output Format :
+All possible strings in different lines
+Constraints :
+1 <= n <= 10^6
+Sample Input:
+23
+Sample Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf
+*/
+
+#include <iostream>
+#include <string>
+#include "solution.h"
+using namespace std;
+
+#include <string.h>
+using namespace std;
+
+string helper(int n)
+{
+    string code[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    return code[n];
+}
+int keypad(int num, string output[])
+{
+
+    if (num == 0 || num == 1)
+    {
+        output[0] = "";
+        return 1;
+    }
+
+    int smalloutput = keypad(num / 10, output);
+    int no = num % 10;
+    int size;
+    string code = helper(no - 2);
+    if (no == 2 || no == 3 || no == 4 || no == 5 || no == 6 || no == 8)
+    {
+        size = 3;
+    }
+    else
+    {
+        size = 4;
+    }
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < smalloutput; j++)
+        {
+            if (i == 0)
+            {
+                output[j] += code[i];
+            }
+            else
+            {
+                output[j + (smalloutput * (size - i))] = output[j];
+                output[j + (smalloutput * (size - i))] += code[i];
+            }
+        }
+    }
+
+    return smalloutput * size;
+}
+
+
+int main(){
+    int num;
+    cin >> num;
+
+    string output[10000];
+    int count = keypad(num, output);
+    for(int i = 0; i < count && i < 10000; i++){
+        cout << output[i] << endl;
+    }
+    return 0;
+}
