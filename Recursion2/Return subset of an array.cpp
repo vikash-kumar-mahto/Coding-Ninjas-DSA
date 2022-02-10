63d744c2d1932117d8391cd915390173b4f8631e
vikash kumar mahto <93486699+vikash-kumar-mahto@users.noreply.github.com>
2022-02-10 12:11:52 +0530

    Create Return subset of an array.cpp

---
+++ Recursion2/Return subset of an array.cpp
@@ -0,0 +1,67 @@
+/*Return subset of an array
+Send Feedback
+Given an integer array (of length n), find and return all the subsets of input array.
+Subsets are of length varying from 0 to n, that contain elements of the array. But the order of elements should remain same as in the input array.
+Note : The order of subsets are not important.
+Input format :
+
+Line 1 : Size of array
+
+Line 2 : Array elements (separated by space)
+
+Sample Input:
+3
+15 20 12
+Sample Output:
+[] (this just represents an empty array, don't worry about the square brackets)
+12 
+20 
+20 12 
+15 
+15 12 
+15 20 
+15 20 12 
+*/
+
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+int subset(int input[], int n, int output[][20])
+{
+    if (n == 0)
+    {
+        output[0][0] = {0};
+        return 1;
+    }
+
+    int c = subset(input, n - 1, output);
+
+    for (int i = 0; i < c; i++)
+    {
+        int size = output[i][0];
+        for (int j = 0; j <= size; j++)
+        {
+            output[i + c][j] = output[i][j];
+        }
+        output[i + c][size + 1] = input[n - 1];
+        output[i + c][0]++;
+    }
+    return c * 2;
+}
+
+int main() {
+  int input[20],length, output[35000][20];
+  cin >> length;
+  for(int i=0; i < length; i++)
+    cin >> input[i];
+  
+  int size = subset(input, length, output);
+
+  for( int i = 0; i < size; i++) { 
+	for( int j = 1; j <= output[i][0]; j++) { 
+		cout << output[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
